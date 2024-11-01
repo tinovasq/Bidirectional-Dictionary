2baab08bed88cbe7e0c5afe726a55f891abec10d
Justino Vasquez <jvasq@proton.me>
2024-11-01 15:22:49 -0500

    add add methods

--- Bidirectional-Dictionary/BiDictionary.cs
+++ Bidirectional-Dictionary/BiDictionary.cs
@@ -1,5 +1,18 @@
 ﻿namespace Bidirectional_Dictionary;
 
-public class BiDictionary
+public class BiDictionary<T1, T2> where T1 : notnull where T2 : notnull
 {
+    private Dictionary<T1, T2> _forward;
+    private Dictionary<T2, T1> _backward;
+    public void Add(T1 key, T2 value)
+    {
+        _forward.Add(key, value);
+        _backward.Add(value, key);
+    }
+
+    public void Add(T2 key, T1 value)
+    {
+        _backward.Add(key, value);
+        _forward.Add(value, key);
+    }
 }
